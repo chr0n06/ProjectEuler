d71f2bb78588b309123a810143e9c142b68dc7bd
chr0n06 <maxime.laniel@outlook.com>
2018-12-25 05:12:34 -0500

    ProjectEuler-Adding solution 1 & 2

---
+++ src/eulerproject/EulerProject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eulerproject;
+
+import BLL.Service;
+
+/**
+ *
+ * @author Maxime
+ */
+public class EulerProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        //Service.problem1_Algo();
+        //Service.problem2_Algo();
+        Service.problem3_Algo(48786);
+    }
+
+}
